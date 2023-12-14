a2cb4b0e850b54c11a0140140acdb432026ba4fe
Igor Carneiro <devigorcarneiro@gmail.com>
2023-12-14 20:02:03 -0300

    feat: main section

--- js/main.js
+++ js/main.js
@@ -21,3 +21,10 @@ function linkAction(){
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+function scrollHeader(){
+    const header = document.getElementById('header')
+    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
