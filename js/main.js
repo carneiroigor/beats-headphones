9368d5fcf5034164deccbd3b34bae45acb9c809a
Igor Carneiro <devigorcarneiro@gmail.com>
2023-12-24 12:39:41 -0300

    feat: scroll up

--- js/main.js
+++ js/main.js
@@ -28,3 +28,10 @@ function scrollHeader(){
     if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
+
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 200 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 200) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
